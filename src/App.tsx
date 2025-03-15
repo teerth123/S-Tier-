93d01d93697c680ab5448d25abcd7ea3d743b791
DexT_17 Teerth <teerth.kulkarni@walchandsangli.ac.in>
2025-03-15 10:46:12 +0530

    boilerplate for TS + Tailwind

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './index.css'
+
+function App() {
+  return (
+    <div className="flex justify-center items-center h-screen bg-gray-100">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Hello, Tailwind!
+      </h1>
+    </div>
+  );
+}
+
+export default App;
